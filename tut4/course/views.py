cb969c3ae1fb9d75dda5f5063124f92e3386fb5f
Danishdbu <mohddanishdbu@gmail.com>
2025-06-11 16:04:05 +0530

    VARIABLE

--- tut4/course/views.py
+++ tut4/course/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# 1- VARIABLE
+
 def learn_dj(req):
     
-    return render(req, 'course/django.html')
+    return render(req, 'course/django.html',context={'name':'Django'})
