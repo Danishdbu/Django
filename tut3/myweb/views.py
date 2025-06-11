303eeac651c2bc6845f90c2d659f05fdc6d72dc4
Danishdbu <mohddanishdbu@gmail.com>
2025-06-11 15:36:51 +0530

    dynamic templates

--- tut3/myweb/views.py
+++ tut3/myweb/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def learn_dj(req):
-    return render(req, 'myweb/django.html')
+    # here we write business logic
+    coursename={'name':'Pyhton'}
+    # return render(req, 'myweb/django.html',context= coursename)
+    # return render(req, 'myweb/django.html',{'name':'Pyhton'})
+    return render(req, 'myweb/django.html',coursename)
     
 
 
