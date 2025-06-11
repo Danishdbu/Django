f96a4d78b412800169a7008a60bc82cc823e61c0
Danishdbu <mohddanishdbu@gmail.com>
2025-06-11 16:57:56 +0530

    Tag

--- tut4/course/views.py
+++ tut4/course/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-
+from datetime import date, datetime
 # Create your views here.
 
 # 1- VARIABLE
 
-def learn_dj(req):
+# def learn_dj(req):
     
-    return render(req, 'course/django.html',context={'name':'Django'})
+#     return render(req, 'course/django.html',context={'name':'Django'})
+
+# 1- FILTER
+# def learn_dj(req):
+#     return render(req, 'course/django.html',context={'name':'Django'})
+
+# Date and Time
+# def learn_dj(req):
+#     d = datetime.now()
+#     return render(req, 'course/django.html',context={'dt':d})
+
+# tag 
+def learn_dj(req):
+    d = datetime.now()
+    return render(req, 'course/django.html',context={'nm':True,'range': range(6)})
