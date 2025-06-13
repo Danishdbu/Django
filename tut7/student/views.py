ed7b03bfaca2c512f20b28783ccb4ac9083347b8
Danishdbu <mohddanishdbu@gmail.com>
2025-06-13 14:45:24 +0530

    retrieve data

--- tut7/student/views.py
+++ tut7/student/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from student.models import Profile
 # Create your views here.
+def home(req):
+    students = Profile.objects.all()
+    return render(req, 'student/all.html',{'students': students})
