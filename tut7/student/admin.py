ca984ae7545f6709a27e924d170f9c3f9cba37e8
Danishdbu <mohddanishdbu@gmail.com>
2025-06-13 15:57:04 +0530

    Django Admin Panel

--- tut7/student/admin.py
+++ tut7/student/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from student.models import Profile,Result
 # Register your models here.
+# without decorator showing data base on admin panel
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id','name','rollNo','city')
+
+# Register the model on admin panel
+admin.site.register(Profile,ProfileAdmin)
+
+# with decorator
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ('id','stud_class','marks')
+
--- tut7/student/models.py
+++ tut7/student/models.py
@@ -6,4 +6,11 @@ class Profile(models.Model):
     email = models.EmailField(max_length=255)
     city = models.CharField(max_length=70)
     rollNo = models.IntegerField()
-    
+    # def __str__(self):
+    #     return self.name
+
+class Result(models.Model):
+    stud_class = models.CharField(max_length=70)
+    marks = models.IntegerField()
+    # def __str__(self):
+    #     return str(self.stud_class)
