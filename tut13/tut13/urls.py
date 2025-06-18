4d85bb55be24b6f42b681d42c637e1619b74d857
Danishdbu <mohddanishdbu@gmail.com>
2025-06-18 14:53:52 +0530

    change  folder

--- tut13/tut13/urls.py
+++ tut13/tut13/urls.py
@@ -1,10 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('account.urls')),
-    path('customer/', include('customer.urls')),
-    path('seller/', include('seller.urls')),
+    
 ]
